5aa03b8c3deaa1ffdc7767d1640420a2a4bb6886
PinnaniMokshagna <pinnanimokshagna@gmail.com>
2025-07-27 01:27:25 +0530

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function About() {
+  return (
+    <>
+
+    <section className="py-5 bg-light text-center">
+      <div className="container">
+        <h2 className="fw-bold mb-3">About Keshav Soft</h2>
+        <p className="lead">Keshav Soft delivers innovative software solutions that empower businesses to grow and succeed. Our team is passionate about technology and committed to excellence.</p>
+        {/* Add more about/team/values sections as needed */}
+      </div>
+    </section>
+    </>
+  );
+}
+
+export default About;
